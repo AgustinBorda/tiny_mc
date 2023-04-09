f10d9e54ce89021796d311342df769761f87b22e
Agustin Borda <aborda@dc.exa.unrc.edu.ar>
2023-04-09 02:14:30 -0300

    added algorithmic optimizations
    Signed-off-by: Agustin Borda <aborda@dc.exa.unrc.edu.ar>

--- tiny_mc.c
+++ tiny_mc.c
@@ -26,6 +26,9 @@ char t3[] = "CPU version, adapted for PEAGPGPU by Gustavo Castellano"
 static float heat[SHELLS];
 static float heat2[SHELLS];
 
+const float albedo = MU_S / (MU_S + MU_A);
+const float shells_per_mfp = 1e4 / MICRONS_PER_SHELL / (MU_A + MU_S);
+
 
 /***
  * Photon
@@ -34,9 +37,6 @@ static float heat2[SHELLS];
 static void photon(void)
 {
  
-    //TODO: Mover esto afuera de la funcion photon
-    const float albedo = MU_S / (MU_S + MU_A);
-    const float shells_per_mfp = 1e4 / MICRONS_PER_SHELL / (MU_A + MU_S);
 
     /* launch */
     float x = 0.0f;
@@ -47,15 +47,42 @@ static void photon(void)
     float w = 1.0f;
     float weight = 1.0f;
 
+    float t = -logf(rand() / (float)RAND_MAX) / (MU_A+MU_S); /* move */
+    z += t * w;
 
-    //TODO: Loop unrolling primera iteracion
+    unsigned int shell = sqrtf(t) * shells_per_mfp; /* absorb */
+    if (shell > SHELLS - 1) {
+       shell = SHELLS - 1;
+     }
+     heat[shell] += (1.0f - albedo) * weight;
+     heat2[shell] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
+     weight *= albedo;
+
+     /* New direction, rejection method */
+     float xi1, xi2;
+     do {
+        //TODO: RAND_MAX - 1.0f se puede poner en una sola variable
+        xi1 = 2.0f * rand() / (float)RAND_MAX - 1.0f;
+        xi2 = 2.0f * rand() / (float)RAND_MAX - 1.0f;
+        t = xi1 * xi1 + xi2 * xi2;
+     } while (1.0f < t);
+     u = 2.0f * t - 1.0f;
+     v = xi1 * sqrtf((1.0f - u * u) / t);
+     w = xi2 * sqrtf((1.0f - u * u) / t);
+
+     if (weight < 0.001f) { /* roulette */
+        if (rand() / (float)RAND_MAX > 0.1f)
+                break;
+        weight /= 0.1f;
+     }
+
     for (;;) {
-        float t = -logf(rand() / (float)RAND_MAX) / (MU_A+MU_S); /* move */
+        t = -logf(rand() / (float)RAND_MAX) / (MU_A+MU_S); /* move */
         x += t * u;
         y += t * v;
         z += t * w;
 
-        unsigned int shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp; /* absorb */
+        shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp; /* absorb */
         if (shell > SHELLS - 1) {
             shell = SHELLS - 1;
         }
@@ -64,7 +91,6 @@ static void photon(void)
         weight *= albedo;
 
         /* New direction, rejection method */
-        float xi1, xi2;
         do {
             //TODO: RAND_MAX - 1.0f se puede poner en una sola variable
             xi1 = 2.0f * rand() / (float)RAND_MAX - 1.0f;
