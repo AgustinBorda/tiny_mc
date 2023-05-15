0d8ad90d470936b781a768435e67d734f7881f5f
CP2023 <cp202336@debian-BULLSEYE-live-builder-AMD64>
2023-05-15 01:57:19 -0300

    backup

--- tiny_mc.c
+++ tiny_mc.c
@@ -1,3 +1,116 @@
+/*  Written in 2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+ *
+ *  To the extent possible under law, the author has dedicated all copyright
+ *  and related and neighboring rights to this software to the public domain
+ *  worldwide. This software is distributed without any warranty.
+ *
+ *  See <http://creativecommons.org/publicdomain/zero/1.0/>. */
+
+
+#define _XOPEN_SOURCE 500  // M_PI
+#define N 8
+
+#include <omp.h>
+#include "params.h"
+#include "wtime.h"
+
+#include <stddef.h>
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <string.h>
+
+/* This is xoshiro512+ 1.0, our generator for floating-point numbers with
+ *    increased state size. We suggest to use its upper bits for
+ *       floating-point generation, as it is slightly faster than xoshiro512**.
+ *          It passes all tests we are aware of except for the lowest three bits,
+ *             which might fail linearity tests (and just those), so if low linear
+ *                complexity is not considered an issue (as it is usually the case) it
+ *                   can be used to generate 64-bit outputs, too.
+ *
+ *                      We suggest to use a sign test to extract a random Boolean value, and
+ *                         right shifts to extract subsets of bits.
+ *
+ *                            The state must be seeded so that it is not everywhere zero. If you have
+ *                               a 64-bit seed, we suggest to seed a splitmix64 generator and use its
+ *                                  output to fill s. */
+
+static inline uint64_t rotl(const uint64_t x, int k) {
+	return (x << k) | (x >> (64 - k));
+}
+
+
+static uint64_t s[8] = {SEED};
+
+uint64_t next(void) {
+	const uint64_t result = s[0] + s[2];
+
+	const uint64_t t = s[1] << 11;
+
+	s[2] ^= s[0];
+	s[5] ^= s[1];
+	s[1] ^= s[2];
+	s[7] ^= s[3];
+	s[3] ^= s[4];
+	s[4] ^= s[5];
+	s[0] ^= s[6];
+	s[6] ^= s[7];
+
+	s[6] ^= t;
+
+	s[7] = rotl(s[7], 21);
+
+	return result;
+}
+
+
+/* This is the jump function for the generator. It is equivalent
+ *    to 2^256 calls to next(); it can be used to generate 2^256
+ *       non-overlapping subsequences for parallel computations. */
+
+void jump(void) {
+	static const uint64_t JUMP[] = { 0x33ed89b6e7a353f9, 0x760083d7955323be, 0x2837f2fbb5f22fae, 0x4b8c5674d309511c, 0xb11ac47a7ba28c25, 0xf1be7667092bcc1c, 0x53851efdb6df0aaf, 0x1ebbc8b23eaf25db };
+
+	uint64_t t[sizeof s / sizeof *s];
+	memset(t, 0, sizeof t);
+	for(int i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+		for(int b = 0; b < 64; b++) {
+			if (JUMP[i] & UINT64_C(1) << b)
+				for(unsigned int w = 0; w < sizeof s / sizeof *s; w++)
+
+					t[w] ^= s[w];
+			next();
+		}
+
+	memcpy(s, t, sizeof s);	
+}
+
+
+/* This is the long-jump function for the generator. It is equivalent to
+ *    2^384 calls to next(); it can be used to generate 2^128 starting points,
+ *       from each of which jump() will generate 2^128 non-overlapping
+ *          subsequences for parallel distributed computations. */
+
+void long_jump(void) {
+	static const uint64_t LONG_JUMP[] = { 0x11467fef8f921d28, 0xa2a819f2e79c8ea8, 0xa8299fc284b3959a, 0xb4d347340ca63ee1, 0x1cb0940bedbff6ce, 0xd956c5c4fa1f8e17, 0x915e38fd4eda93bc, 0x5b3ccdfa5d7daca5 };
+
+	uint64_t t[sizeof s / sizeof *s];
+	memset(t, 0, sizeof t);
+	for(int i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
+		for(int b = 0; b < 64; b++) {
+			if (LONG_JUMP[i] & UINT64_C(1) << b)
+				for(unsigned int w = 0; w < sizeof s / sizeof *s; w++)
+					t[w] ^= s[w];
+			next();
+		}
+
+	memcpy(s, t, sizeof s);	
+}
+
+
 /* Tiny Monte Carlo by Scott Prahl (http://omlc.ogi.edu)"
  * 1 W Point Source Heating in Infinite Isotropic Scattering Medium
  * http://omlc.ogi.edu/software/mc/tiny_mc.c
@@ -5,44 +118,68 @@
  * Adaptado para CP2014, Nicolas Wolovick
  */
 
-#define _XOPEN_SOURCE 500  // M_PI
-
-#include "params.h"
-#include "wtime.h"
-
-#include <assert.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-//TODO: Comentar esto
 char t1[] = "Tiny Monte Carlo by Scott Prahl (http://omlc.ogi.edu)";
 char t2[] = "1 W Point Source Heating in Infinite Isotropic Scattering Medium";
 char t3[] = "CPU version, adapted for PEAGPGPU by Gustavo Castellano"
-            " and Nicolas Wolovick";
+" and Nicolas Wolovick";
 
 
-static unsigned int g_seed;
+
+
+struct photon {
+	float x;
+	float y;
+	float z;
+	float u;
+	float v;
+	float w;
+	float weight;
+	float t;
+	float xi1;
+	float xi2;
+	unsigned int shell;
+	float heat[SHELLS];
+	float heat2[SHELLS];
+};
+
+//N photons:
+float x[N], y[N], z[N], u[N], v[N], w[N], weight[N], t[N], xi1[N], xi2[N];
+
+float heat[SHELLS];
+float heat2[SHELLS];
+
+void make_photons(void) {
+	for (size_t i=0; i<N; ++i) {
+		w[i] = 1.0f;
+		weight[i] = 1.0f;
+	}
+}
+
+
+static unsigned int g_seed = SEED;
 
 // global state, heat and heat square in each shell
-static float heat[SHELLS];
-static float heat2[SHELLS];
+
 
 
 //################  FUNCIONES PARA FAST_RAND() ###########
 
 // Used to seed the generator. 
-          
+
 void fast_srand(int seed) {
-    g_seed = seed;
+	g_seed = seed;
 }
 
 // Compute a pseudorandom integer.
 // Output value in range [0, 32767]
 
-int fast_rand(void) {
-    g_seed = (214013*g_seed+2531011);
-    return (g_seed>>16)&0x7FFF;
+void fast_rand(int* restrict a, const size_t n, unsigned int* seed) {
+	unsigned int s = *seed;
+	for (size_t i=0; i<n; i++) {
+		s = (214013*(s)+2531011);
+		a[i] = (s>>16)&0x7FFF;
+	}
+	*seed = s;
 }    
 //##########################################################
 
@@ -50,116 +187,94 @@ int fast_rand(void) {
 const float albedo = MU_S / (MU_S + MU_A);
 const float shells_per_mfp = 1e4 / MICRONS_PER_SHELL / (MU_A + MU_S);
 
-
 /***
  * Photon
  ***/
 
 static void photon(void)
 {
- 
 
-    /* launch */
-    float x = 0.0f;
-    float y = 0.0f;
-    float z = 0.0f;
-    float u = 0.0f;
-    float v = 0.0f;
-    float w = 1.0f;
-    float weight = 1.0f;
+	for(unsigned int j=0;j<64;j++) {
+		float weight = 1.0f;
+		for(size_t i=0; i<N; i++) {
+			t[i] = -logf(42 / (float)32767.0) / (MU_A+MU_S); /* move */
+			x[i] += t[i] * u[i];
+			y[i] += t[i] * v[i];
+			z[i] += t[i] * w[i];
 
-    float t = -logf(fast_rand() / (float)32767.0) / (MU_A+MU_S); /* move */
-    z += t * w;
+		}
 
-    unsigned int shell = sqrtf(t) * shells_per_mfp; /* absorb */
-    if (shell > SHELLS - 1) {
-       shell = SHELLS - 1;
-     }
-     heat[shell] += (1.0f - albedo) * weight;
-     heat2[shell] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
-     weight *= albedo;
-
-     /* New direction, rejection method */
-     float xi1, xi2;
-     do {
-        xi1 = 2.0f * fast_rand() / (float)32767.0 - 1.0f;
-        xi2 = 2.0f * fast_rand() / (float)32767.0 - 1.0f;
-        t = xi1 * xi1 + xi2 * xi2;
-     } while (1.0f < t);
-     u = 2.0f * t - 1.0f;
-     v = xi1 * sqrtf((1.0f - u * u) / t);
-     w = xi2 * sqrtf((1.0f - u * u) / t);
+		for(size_t i=0; i<N; i++) {
+			unsigned int shell = sqrtf(x[i] * x[i] + y[i] * y[i] + z[i] * z[i]) * shells_per_mfp; /* absorb */
+			shell = (shell > SHELLS-1) ? SHELLS-1 : shell;
+			heat[shell] += (1.0f - albedo) * weight;
+			heat2[shell] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
+		}
+		weight *= albedo;
 
 
-    for (;;) {
-        t = -logf(fast_rand() / (float)32767.0) / (MU_A+MU_S); /* move */
-        x += t * u;
-        y += t * v;
-        z += t * w;
+		for(size_t i=0; i<N; i++) {
+			do {
+				xi1[i] = 2.0f * 42 / (float)32767.0 - 1.0f;
+				xi2[i] = 2.0f * 42 / (float)32767.0 - 1.0f;
+				t[i] = xi1[i] * xi1[i] + xi2[i] * xi2[i];
+			} while (1.0f < t[i]);
+		}
 
-        shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp; /* absorb */
-        if (shell > SHELLS - 1) {
-            shell = SHELLS - 1;
-        }
-        heat[shell] += (1.0f - albedo) * weight;
-        heat2[shell] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
-        weight *= albedo;
+		for(size_t i=0; i<N; i++) {
+			u[i] = 2.0f * t[i] - 1.0f;
+			v[i] = xi1[i] * sqrtf((1.0f - u[i] * u[i]) / t[i]);
+			w[i] = xi2[i] * sqrtf((1.0f - u[i] * u[i]) / t[i]);
+		}
+	}
 
-        if (weight < 0.001f) { /* roulette */
-            if (fast_rand() / (float)32767.0 > 0.1f) //TODO: ver que tanto se puede meter mano aca
-                break;
-            weight /= 0.1f; //TODO: cambiar division por multiplicacion
-        }
-        /* New direction, rejection method */
-        do {
-            xi1 = 2.0f * fast_rand() / (float)32767.0 - 1.0f;
-            xi2 = 2.0f * fast_rand() / (float)32767.0 - 1.0f;
-            t = xi1 * xi1 + xi2 * xi2;
-        } while (1.0f < t);
-        u = 2.0f * t - 1.0f;
-        v = xi1 * sqrtf((1.0f - u * u) / t);
-        w = xi2 * sqrtf((1.0f - u * u) / t);
-    }
 }
 
 
+void move_photons_no_roullete() {
+}
 /***
  * Main matter
  ***/
 
 int main(void)
 {
-    // heading
-    printf("# %s\n# %s\n# %s\n", t1, t2, t3);
-    printf("# Scattering = %8.3f/cm\n", MU_S);
-    printf("# Absorption = %8.3f/cm\n", MU_A);
-    printf("# Photons    = %8d\n#\n", PHOTONS);
+	photon();
+	//long_jump();
+	for(int i=0; i< 64; i++) {
+		printf("%" PRIu64 "\n", s[i]);
+	}
+	// heading
+	/*	printf("# %s\n# %s\n# %s\n", t1, t2, t3);
+		printf("# Scattering = %8.3f/cm\n", MU_S);
+		printf("# Absorption = %8.3f/cm\n", MU_A);
+		printf("# Photons    = %8d\n#\n", PHOTONS);
 
-    // configure RNG
-    fast_srand(SEED);
-    // start timer
-    double start = wtime();
-    // simulation
-    for (unsigned int i = 0; i < PHOTONS; ++i) {
-        photon();
-    }
-    // stop timer
-    double end = wtime();
-    assert(start <= end);
-    double elapsed = end - start;
+	// configure RNG
+	fast_srand(SEED);
+	// start timer
+	double start = wtime();
+	// simulation
+	for (unsigned int i = 0; i < PHOTONS; ++i) {
+	photon();
+	}
+	// stop timer
+	double end = wtime();
+	assert(start <= end);
+	double elapsed = end - start;
 
-    printf("# %lf seconds\n", elapsed);
-    printf("# %lf K photons per second\n", 1e-3 * PHOTONS / elapsed);
+	printf("# %lf seconds\n", elapsed);
+	printf("# %lf K photons per second\n", 1e-3 * PHOTONS / elapsed);
 
-    printf("# Radius\tHeat\n");
-    printf("# [microns]\t[W/cm^3]\tError\n");
-    float t = 4.0f * M_PI * powf(MICRONS_PER_SHELL, 3.0f) * PHOTONS / 1e12;
-    for (unsigned int i = 0; i < SHELLS - 1; ++i) {
-        printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
-               heat[i] / t / (i * i + i + 1.0 / 3.0),
-               sqrt(heat2[i] - heat[i] * heat[i] / PHOTONS) / t / (i * i + i + 1.0f / 3.0f));
-    }
-    printf("# extra\t%12.5f\n", heat[SHELLS - 1] / PHOTONS);
-
-    return 0;
+	printf("# Radius\tHeat\n");
+	printf("# [microns]\t[W/cm^3]\tError\n");
+	float t = 4.0f * M_PI * powf(MICRONS_PER_SHELL, 3.0f) * PHOTONS / 1e12;
+	for (unsigned int i = 0; i < SHELLS - 1; ++i) {
+	printf("%6.0f\t%12.5f\t%12.5f\n", i * (float)MICRONS_PER_SHELL,
+	heat[i] / t / (i * i + i + 1.0 / 3.0),
+	sqrt(heat2[i] - heat[i] * heat[i] / PHOTONS) / t / (i * i + i + 1.0f / 3.0f));
+	}
+	printf("# extra\t%12.5f\n", heat[SHELLS - 1] / PHOTONS);
+	*/
+	return 0;
 }
